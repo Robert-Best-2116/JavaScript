913d80cf2931f7c1840043ed72ff5adc09aff499
Robert-Best-2116 <Robert.Best2116@gmail.com>
2023-02-19 19:31:15 -0800

    Adjusted 'Prop It Up' to 'Putting It Together'

--- Projects/Prop It Up/prop-it-up/src/App.js
+++ Projects/Prop It Up/prop-it-up/src/App.js
@@ -1,14 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
 import PersonCard from './components/PersonCard';
 
 function App() {
   return (
     <div className="App">
-      <PersonCard firstName={"Robert"} lastName={"Best"} age={"31"} hairColor={"Black"}/>
-      <PersonCard firstName={"Haylee"} lastName={"Jones"} age={"23"} hairColor={"Black"}/>
-      <PersonCard firstName={"Larry"} lastName={"Roiem"} age={"??"} hairColor={"Blond"}/>
-      <PersonCard firstName={"Peter"} lastName={"Jewel"} age={"34"} hairColor={"Brown"}/>
+      <PersonCard firstName={"Robert"} lastName={"Best"} age={31} hairColor={"Black"}/>
+      <PersonCard firstName={"Haylee"} lastName={"Jones"} age={23} hairColor={"Black"}/>
+      <PersonCard firstName={"Larry"} lastName={"Roiem"} age={54} hairColor={"Blond"}/>
+      <PersonCard firstName={"Peter"} lastName={"Jewel"} age={34} hairColor={"Brown"}/>
+      
     </div>
   );
 }
--- Projects/Prop It Up/prop-it-up/src/components/PersonCard.js
+++ Projects/Prop It Up/prop-it-up/src/components/PersonCard.js
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react';
 
 const PersonCard = (props) => {
+    const{firstName, lastName, hairColor, age } = props
+    
+    const [getAge, setAge] = useState(age)
+
+    const addAge = () => {
+        setAge(getAge + 1)
+    }
+
   return (
     <div>
-        <h1> {props.lastName}, {props.firstName} </h1> 
-        <p>Age: {props.age} </p>
-        <p>Hair Color: {props.hairColor}</p>
+        <h1> {lastName}, {firstName} </h1> 
+        <p>Age: {getAge} </p>
+        <p>Hair Color: {hairColor}</p>
+        <button onClick={addAge}> {firstName} Birthday Button! </button>
     </div>
   )
 }
 
-export default PersonCard
+export default PersonCard;
