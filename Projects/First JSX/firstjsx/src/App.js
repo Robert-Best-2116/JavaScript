e12aff297944790aa8abe5f88272bfcc7deca9ad
Robert-Best-2116 <Robert.Best2116@gmail.com>
2023-02-19 17:11:35 -0800

    First React Upload

---
+++ Projects/First JSX/firstjsx/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Hello Dojo!</h1>
+      <h2>Things I Need To Do!!!!</h2>
+      <ul>Learn React</ul>
+      <ul>Do My Homework</ul>
+      <ul>Do Discussion Questions!</ul>
+    </div>
+  );
+}
+
+export default App;
