b317b11d2ad26dea6e026c2b331e13ab2aabd144
Robert-Best-2116 <Robert.Best2116@gmail.com>
2023-03-17 20:41:15 -0700

    Full Stack Mern Demo Project

---
+++ Projects/FullStackMern/server/controllers/person.controller.js
@@ -0,0 +1,30 @@
+
+
+const Person = require('../models/person.model');    /* this is new */
+module.exports.index = (request, response) => {
+    response.json({
+        message: "Hello World"
+    });
+}
+          /* The method below is new */
+module.exports.createPerson = (request, response) => {
+    // Mongoose's "create" method is run using our Person model to add a new person to our db's person collection.
+    // request.body will contain something like {firstName: "Billy", lastName: "Washington"} from the client
+    Person.create(request.body) //This will use whatever the body of the client's request sends over
+        .then(person => response.json(person))
+        .catch(err => response.json(err));
+}
+
+
+
+module.exports.getAllPeople = (request, response) => {
+    Person.find({})
+        .then(allPersons => {
+            console.log(allPersons); //console logs are optional, but they are highly recommended for troubleshooting!
+            response.json({persons : allPersons});
+        })
+        .catch(err => {
+            console.log(err)
+            response.json(err)
+        })
+}
